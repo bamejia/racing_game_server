acf0342b696a96c5a00f2e0b720ea94d8f525fd9
bamejia <bamejia@udel.edu>
2020-02-07 08:40:45 -0500

    removed build, since it's not needed

--- build/server-v0.3/online_multiplayer/waiting_screen_view.py
+++
@@ -1,4 +0,0 @@
-import pygame
-import global_variables as gv
-
-
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sys
 buildOptions = dict(
     packages=["sys", 'pygame'],
     include_files=["controller", "model", "global_variables.py", 'online_multiplayer'],
-    build_exe=".\\build\\server-v0.3",
+    build_exe=".\\build\\server-v0.4",
     excludes=[]
 )
 
@@ -19,7 +19,7 @@ executables = [
 ]
 
 setup(name='racing game',
-      version='0.3',
+      version='0.4',
       description='racing game',
       options=dict(build_exe=buildOptions),
       executables=executables, requires=['pygame'])
